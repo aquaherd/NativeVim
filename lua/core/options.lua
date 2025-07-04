2beab844dd75d37be6912c530f9de6b234d544ed
Hakan Erduman <hakan.erduman@siemens-healthineers.com>
2025-07-04 07:16:03 +0200

    Better OSC paste

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -62,15 +62,21 @@ vim.g.netrw_banner = 0
 -- clipboard use local and ssh
 vim.opt.clipboard = 'unnamedplus'
 if vim.env.SSH_TTY then
-	vim.g.clipboard = {
-		name = 'OSC 52',
-		copy = {
-			['+'] = require('vim.ui.clipboard.osc52').copy '+',
-			['*'] = require('vim.ui.clipboard.osc52').copy '*',
-		},
-		paste = {
-			['+'] = require('vim.ui.clipboard.osc52').paste '+',
-			['*'] = require('vim.ui.clipboard.osc52').paste '*',
-		},
-	}
+	local function paste() return {
+		vim.split(vim.fn.getreg(''), '\n'),
+		vim.fn.getregtype(''),
+	} end
+	if vim.env.SSH_TTY then
+		vim.g.clipboard = {
+			name = 'OSC 52',
+			copy = {
+				['+'] = require('vim.ui.clipboard.osc52').copy '+',
+				['*'] = require('vim.ui.clipboard.osc52').copy '*',
+			},
+			paste = {
+				['+'] = paste,
+				['*'] = paste,
+			},
+		}
+	end
 end
